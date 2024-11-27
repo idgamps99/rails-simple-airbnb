6a63ff3df5a4f1197be29cfc246482cad5139959
Myles Amphlett <mylesamphlett@gmail.com>
2024-11-27 21:35:29 +0000

    added controller and index route and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Starting seeds...."
+
+4.times do
+  Flat.create!(
+    name: Faker::Quote.famous_last_words,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.mitch_hedberg,
+    price_per_night: rand(50..250),
+    number_of_guests: rand(1..14)
+  )
+end
+
+puts "Finished seeding!"
